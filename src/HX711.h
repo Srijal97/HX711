35a9af9ba374e63474d0b37754462b9b464d5d7a
Srijal97 <srijal97@gmail.com>
2019-03-17 16:29:22 +0530

    Added modified libraries and example code for non-blocking read from HX711

--- src/HX711.h
+++ src/HX711.h
@@ -1,12 +1,3 @@
-/**
- *
- * HX711 library for Arduino
- * https://github.com/bogde/HX711
- *
- * MIT License
- * (c) 2018 Bogdan Necula
- *
-**/
 #ifndef HX711_h
 #define HX711_h
 
@@ -22,31 +13,21 @@ class HX711
 		byte PD_SCK;	// Power Down and Serial Clock Input Pin
 		byte DOUT;		// Serial Data Output Pin
 		byte GAIN;		// amplification factor
-		long OFFSET = 0;	// used for tare weight
-		float SCALE = 1;	// used to return weight in grams, kg, ounces, whatever
+		long OFFSET;	// used for tare weight
+		float SCALE;	// used to return weight in grams, kg, ounces, whatever
 
 	public:
-
-		HX711();
+		// define clock and data pin, channel, and gain factor
+		// channel selection is made by passing the appropriate gain: 128 or 64 for channel A, 32 for channel B
+		// gain: 128 or 64 for channel A; channel B works with 32 gain factor only
+		HX711(byte dout, byte pd_sck, byte gain = 128);
 
 		virtual ~HX711();
 
-		// Initialize library with data output pin, clock input pin and gain factor.
-		// Channel selection is made by passing the appropriate gain:
-		// - With a gain factor of 64 or 128, channel A is selected
-		// - With a gain factor of 32, channel B is selected
-		// The library default is "128" (Channel A).
-		void begin(byte dout, byte pd_sck, byte gain = 128);
-
-		// Check if HX711 is ready
+		// check if HX711 is ready
 		// from the datasheet: When output data is not ready for retrieval, digital output pin DOUT is high. Serial clock
 		// input PD_SCK should be low. When DOUT goes to low, it indicates data is ready for retrieval.
 		bool is_ready();
-
-		// Wait for the HX711 to become ready
-		void wait_ready(unsigned long delay_ms = 0);
-		bool wait_ready_retry(int retries = 3, unsigned long delay_ms = 0);
-		bool wait_ready_timeout(unsigned long timeout = 1000, unsigned long delay_ms = 0);
 
 		// set the gain factor; takes effect only after a call to read()
 		// channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
@@ -55,6 +36,11 @@ class HX711
 
 		// waits for the chip to be ready and returns a reading
 		long read();
+
+		// read() causes a lot of delay (~100ms for Uno) and causes Blocking as it waits for chip to be ready
+		// read_direct() does not check the chip status and requires it to be ready beforehand.
+		// Call only if chip is_ready()
+		long read_direct();
 
 		// returns an average reading; times = how many times to read
 		long read_average(byte times = 10);
@@ -65,6 +51,9 @@ class HX711
 		// returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
 		// times = how many readings to do
 		float get_units(byte times = 1);
+
+		//see read_direct() above.
+		float get_units_direct();
 
 		// set the OFFSET value for tare weight; times = how many times to read the tare value
 		void tare(byte times = 10);
